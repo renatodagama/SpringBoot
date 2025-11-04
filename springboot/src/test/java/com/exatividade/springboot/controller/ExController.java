0e8f2df0f06cd517c302991c8b155582f9d899fe
renatodagama <32046927+renatodagama@users.noreply.github.com>
2025-11-04 22:22:01 +0000

    springBoot

--- springboot/src/test/java/com/exatividade/springboot/controller/ExController.java
+++ springboot/src/test/java/com/exatividade/springboot/controller/ExController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/")
-
 public class ExController {
 
     @GetMapping("/api")
@@ -16,13 +14,10 @@ public class ExController {
         return "Olá, aplicação Spring!";
     }
 
-    /**
-     * @param numUm
-     * @return
-     */
+    //  soma +10 e verifica Par/Ímpar
     @GetMapping("/api/teste/{numUm}")
     public String teste(@PathVariable int numUm) {
-       
+        
         int resultadoSoma = numUm + 10;
         
         String parOuImpar;
@@ -33,8 +28,29 @@ public class ExController {
             parOuImpar = "Ímpar";
         }
         
-        // 3. Retorna a soma e o status Par/Ímpar
-        return "O resultado da soma é: " + resultadoSoma + ". É um número " + parOuImpar + ".";
+        return "O resultado da soma (com 10) é: " + resultadoSoma + ". É um número " + parOuImpar + ".";
     }
 
+    //api/soma/5/3
+     
+    @GetMapping("/api/soma/{num1}/{num2}")
+    public String somar(@PathVariable int num1, @PathVariable int num2) {
+        int resultado = num1 + num2;
+        return "A soma de " + num1 + " + " + num2 + " é: " + resultado + ".";
+    }
+
+    //api/subtracao/10/4
+    @GetMapping("/api/subtracao/{num1}/{num2}")
+    public String subtrair(@PathVariable int num1, @PathVariable int num2) {
+        int resultado = num1 - num2;
+        return "A subtração de " + num1 + " - " + num2 + " é: " + resultado + ".";
+    }
+
+    // api/verificanumeroparouimpar/7
+     
+    @GetMapping("/api/verificanumeroparouimpar/{numero}")
+    public String verificaParOuImpar(@PathVariable int numero) {
+        String status = (numero % 2 == 0) ? "Par" : "Ímpar";
+        return "O número " + numero + " é " + status + ".";
+    }
 }
